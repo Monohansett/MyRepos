5ffb9af116acf939175ffccf6a047f22797d8eb6
Kiryl Navumenka <k.navumenka@gmail.com>
2017-03-29 15:44:57 +0300

    secons

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -16,9 +16,7 @@ angular.module('Eggly', [
             {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
             {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
             {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-            {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-            {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-            {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+            {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" }
         ];
 
         $scope.isCreating = false;
